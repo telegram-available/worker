ab80ae8bffd83c9345f7a80bbf7709eb6dcd62aa
telegram-available <77942703+telegram-available@users.noreply.github.com>
2021-01-25 23:35:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import stat
 import shutil
-import _thread
 import objects
 from git.repo.base import Repo
 # ========================================================================================================
@@ -14,14 +13,14 @@ def delete(action, name, exc):
     return action, name, exc
 
 
-Repo.clone_from('https://github.com/evolvestin/Telegram-Username-Availability-Parser', 'temp')
-shutil.copy('temp/test.py', 'functions.py')
+Repo.clone_from('https://github.com/evolvestin/Telegram-Username-Availability-Parser/', 'temp')
+for file_name in os.listdir('temp/worker'):
+    shutil.copy(f'temp/worker/{file_name}', file_name)
 shutil.rmtree('temp', onerror=delete)
 # ========================================================================================================
-print(f'Запуск за {objects.time_now() - stamp1} секунд(ы)')
+print(f'Запуск оболочки за {objects.time_now() - stamp1} секунды')
 
 
 if __name__ == '__main__':
-    from functions import checking, files_upload
-    _thread.start_new_thread(files_upload, ())
-    checking()
+    from functions import start
+    start()
